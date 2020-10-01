ebee467233c30a400aa041eeb6aaac3b6f12bac9
joy-dey <36854712+joy-dey@users.noreply.github.com>
2020-10-01 20:45:56 +0530

    fixed js error and added nav shadow on scroll for better differentiation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,9 @@
 const dateSelector = document.querySelector('.date-display'),
-    navBar = document.querySelector('.navbar-dark');
-dateSelector.innerText = new Date().toLocaleDateString('UTC', { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' });
+    navBar = document.querySelector('.navbar-dark'),
+    newsUpdates = document.querySelector('.live-portal');
+if (newsUpdates) {
+    dateSelector.innerText = new Date().toLocaleDateString('UTC', { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' });
+}
 
 window.addEventListener('scroll', () => {
     if (window.pageYOffset > 130) {
